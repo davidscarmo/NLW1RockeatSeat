edf71c418baa51a187d33f39ac7f61c97ac67b73
David Carmo <davidsc2812@gmail.com>
2020-08-03 07:42:34 -0300

    Upload FInal
    up final dos arquivos

---
+++ web/src/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'; 
+
+
+interface HeaderProps
+{
+    title: String; 
+} 
+
+
+const Header: React.FC<HeaderProps> = (props) => 
+{
+    return(
+        <header>
+            <h1>{props.title}</h1>
+        </header>
+    );
+}
+
+export default Header;
